9b7b61b8cded2a6c355277cd6d97b30a01b28d53
Jared Weiss <j@lapeno.xyz>
2016-07-07 01:15:00 -0400

    Stubbing out graspit commander

--- src/graspit_commander/graspit_commander.py
+++ src/graspit_commander/graspit_commander.py
@@ -6,12 +6,53 @@ class GraspitCommander(object):
     """
     Python interface for interacting with GraspIt
     """
-   
+
     @staticmethod
-    def getRobot(i):
+    def getRobots():
+        pass
+
+    @staticmethod
+    def getRobot(id):
         rospy.wait_for_service('getRobot')
         
         serviceProxy = rospy.ServiceProxy('getRobot', GetRobot)
-        robot = serviceProxy(i)
+        robot = serviceProxy(id)
 
         return robot
+
+    @staticmethod
+    def setRobotPose(id, pose):
+        pass
+
+    @staticmethod
+    def getGraspableBodies():
+        pass
+
+    @staticmethod
+    def getGraspableBody(id):
+        pass
+
+    @staticmethod
+    def setGraspableBodyPose(id, pose):
+        pass
+
+    @staticmethod
+    def getObstacles():
+        pass
+
+    @staticmethod
+    def getObstacle(id):
+        pass
+
+    @staticmethod
+    def setObstaclePose(id, pose):
+        pass
+
+    @staticmethod
+    def getDynamics():
+        pass
+
+    @staticmethod
+    def setDynamics(enabled):
+        pass
+
